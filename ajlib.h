b0b6517ae7634edd3b53386be9d0bca0b93d6598
AlfonsoJan <alfonsobusker@gmail.com>
2026-03-31 18:40:27 +0200

    created macro for function linkage

---
+++ ajlib.h
@@ -0,0 +1,12 @@
+// ajlib - v0.1.0 - Public Domain - https://github.com/AlfonsoJan/ajlib.h
+
+#ifndef AJLIB_H_
+#define AJLIB_H_
+
+// Define AJLIB_DEF before including this file to control function linkage.
+// Example: #define AJLIB_DEF static inline
+#ifndef AJLIB_DEF
+#define AJLIB_DEF
+#endif // AJLIB_DEF
+
+#endif // AJLIB_H_
